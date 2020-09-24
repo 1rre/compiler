a275955363de060d2ec87d9120385a66eb280dd5
tjm1518 <tjm1518@ic.ac.uk>
2020-09-24 20:49:18 +0100

    \

--- lib/c_py.ex
+++ lib/c_py.ex
@@ -1,11 +1,9 @@
-defmodule C_Py.Helpers do
+defmodule C_Py do
   import NimbleParsec
 
-  def whitespace(combinator \\ empty()) do
-    combinator |> ignore(repeat(choice([ascii_char(' '), ascii_char('\t'), ascii_char('\n')])))
-  end
-  def int_literal(combinator \\ empty()) do
-    combinator |> choice([optional(ascii_char('-')) |> repeat(ascii_char('0')) |> choice([
+  defparsecp(:whitespace, ignore(repeat(choice([ascii_char(' '), ascii_char('\t'), ascii_char('\n')]))))
+
+  defparsecp(:int_literal, choice([optional(ascii_char('-')) |> repeat(ascii_char('0')) |> choice([
       optional(ascii_char([?0..?1])) |> integer(max: 9),
       ascii_char('2') |> choice([
         ascii_char('0') |> integer(8),
@@ -33,77 +31,121 @@ defmodule C_Py.Helpers do
         ])
       ])
     ]), ascii_char('-') |> repeat(ascii_char('0')) |> string("2147483648")])
-  end
+  )
 
-  def int_param(combinator \\ empty()) do
-    combinator
-    |> whitespace()
+  defparsecp(:variable_name,
+    choice([ascii_char('_'), ascii_char([?a..?z] ++ [?A..?Z])])
+    |> repeat(choice([ascii_char('_'), ascii_char([?a..?z] ++ [?A..?Z] ++ [?0..?9])]))
+  )
+
+  defparsecp(:int,
+    parsec(:whitespace)
     |> string("int")
-    |> whitespace()
-    |> ascii_char([?a..?z] ++ [?A..?Z])
-    |> repeat(ascii_char([?a..?z] ++ [?A..?Z] ++ [?0..?9]))
-    |> whitespace()
-  end
+    |> parsec(:whitespace)
+    |> parsec(:variable_name)
+    |> parsec(:whitespace)
+  )
 
-  def int_decl(combinator \\ empty()) do
-    combinator
-    |> int_param()
-    |> whitespace()
+  defparsecp(:int_decl,
+    parsec(:whitespace)
+    |> parsec(:variable_name)
+    |> parsec(:whitespace)
     |> ascii_char('=')
-    |> whitespace()
-    |> int_literal()
-    |> whitespace()
+    |> parsec(:whitespace)
+    |> parsec(:int_literal)
+    |> parsec(:whitespace)
     |> ascii_char(';')
-    |> whitespace()
-  end
+    |> parsec(:whitespace)
+  )
 
-  def line(combinator \\ empty()) do
-    combinator
-    |> whitespace()
+  defparsecp(:operation,
+    parsec(:variable_name)
+  )
+
+  defparsecp(:declaration,
+    ignore(string("int"))
+    |> parsec(:whitespace)
+    |> parsec(:variable_name)
+    |> ignore(ascii_char(';'))
+  )
+
+  defparsecp(:assignment,
+    parsec(:declaration)
+    |> parsec(:whitespace)
+    |> ascii_char('=')
+    |> parsec(:whitespace)
+    |> choice([parsec(:int_literal), parsec(:operation)])
+    |> parsec(:whitespace)
     |> ascii_char(';')
-    |> whitespace()
-  end
+    |> parsec(:whitespace)
+  )
 
-  def int_func(combinator \\ empty()) do
-    combinator
-    |> whitespace()
-    |> string("int")
-    |> whitespace()
-    |> ignore(ascii_char('('))
-    |> optional(repeat(whitespace() |> int_param() |> whitespace() |> ignore(ascii_char(',')) |> whitespace()) |> int_param())
-    |> whitespace()
-    |> ignore(ascii_char(')'))
-    |> whitespace()
-    |> ascii_char('{')
-    |> ignore(repeat(ascii_char('\n')))
-    |> repeat(line())
-    |> ascii_char('}')
-  end
+  defparsecp(:instruction,
+    parsec(:whitespace)
+    |> ascii_char(';')
+    |> parsec(:whitespace)
+  )
 
-  def program(combinator \\ empty()) do
-    combinator
-    |> whitespace()
-    |> repeat(choice([int_func(), line()]) |> whitespace())
-  end
-
-  def boolean_expression(combinator \\ empty()) do
-    combinator
-  end
-
-  def if_statement(combinator \\ empty()) do
-    combinator
-    |> ignore(string("if") |> whitespace() |> ascii_char('(') |> whitespace())
-    |> boolean_expression()
-    |> ignore(whitespace() |> ascii_char('{'))
-    |> repeat(line())
+  defparsecp(:code_block,
+    parsec(:whitespace)
+    |> ignore(ascii_char('{'))
+    |> repeat(parsec(:instruction))
     |> ignore(ascii_char('}'))
-    |> whitespace()
-  end
+    |> parsec(:whitespace)
+  )
 
-end
-defmodule C_Py do
-  import NimbleParsec
-  import C_Py.Helpers
-  defparsec(:int_literal, int_literal())
-  defparsec(:int_decl, int_decl())
+  defparsecp(:int_func,
+    parsec(:whitespace)
+    |> string("int")
+    |> parsec(:whitespace)
+    |> ignore(ascii_char('('))
+    |> optional(repeat(parsec(:whitespace) |> parsec(:int) |> parsec(:whitespace) |> ignore(ascii_char(',')) |> parsec(:whitespace)) |> parsec(:int))
+    |> parsec(:whitespace)
+    |> ignore(ascii_char(')'))
+    |> parsec(:code_block)
+  )
+
+  defparsecp(:comparison,
+    choice([parsec(:variable_name), parsec(:int_literal)])
+    |> parsec(:whitespace)
+    |> choice([string("=="), string(">"), string("<"), string(">="), string("<=")])
+    |> parsec(:whitespace)
+    |> choice([parsec(:variable_name), parsec(:int_literal)])
+  )
+
+  defparsecp(:boolean_expression,
+    ignore(repeat(ascii_char('') |> eventually(ascii_char(''))))
+    |> parsec(:comparison)
+    |> repeat(choice([string("&&"), string("||")]) |> parsec(:boolean_expression))
+    |> ignore(optional(ascii_char('')))
+  )
+
+  defparsecp(:if_statement,
+    ignore(string("if") |> parsec(:whitespace) |> ascii_char('(') |> parsec(:whitespace))
+    |> choice([parsec(:boolean_expression), string("true"), string("false")])
+    |> ignore(parsec(:whitespace) |> ascii_char(')'))
+    |> parsec(:code_block)
+  )
+
+  defparsec(:if_else_statement,
+    parsec(:if_statement)
+    |> repeat(string("else") |> parsec(:if_statement))
+    |> optional(string("else") |> parsec(:code_block))
+  )
+
+  defparsec(:for_loop,
+    ignore(string("for") |> parsec(:whitespace) |> ascii_char('('))
+    |> parsec(:instruction)
+    |> parsec(:boolean_expression)
+    |> ignore(ascii_char(';'))
+    |> parsec(:operation)
+  )
+
+
+
+  defparsec(:program,
+    parsec(:whitespace)
+    |> repeat(choice([parsec(:int_func), parsec(:instruction)]) |> parsec(:whitespace))
+  )
+
 end
