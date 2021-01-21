50a1ee585b8b4b6cf4f13b0db04e7d90d90dbc7e
tjm1518 <info@tmoor.es>
2021-01-21 22:07:29 +0000

    preprocessor nearly handles #define

--- Translator/src/es/tmoor/compiler/Main.scala
+++ Translator/src/es/tmoor/compiler/Main.scala
@@ -1,7 +1,10 @@
 package es.tmoor.compiler
 
 object Main extends App with Preprocessor {
-  val input = io.Source.fromFile("test/test.c")
-  test
-  println(parse(defineP, "hello;"))
+  val input = io.Source.fromFile("test/test.c").getLines
+//  parse(program, input.next)
+//  println(defines.mkString(","))
+//  println(parse(defineP.get, "console(\"hello\")"))
+  
+  println(input.map(ln => parse(program, ln).getOrElse(sys.error("Oh noes!"))).mkString("\n"))
 }
--- Translator/src/es/tmoor/compiler/Preprocessor.scala
+++ Translator/src/es/tmoor/compiler/Preprocessor.scala
@@ -4,34 +4,50 @@ import util.parsing.combinator._
 import collection.mutable.HashSet
 
 trait Preprocessor extends RegexParsers {
+  val defines = HashSet[Define]()
+
   override def skipWhitespace: Boolean = true
-  private class Define(name: String, definition: String) {
+
+  class Define(name: String, definition: String) {
     def parser = name.r ^^ { _ => definition }
+    override def toString = name
   }
 
-  private class ArgDefine(name: String, args: List[String], definition: String) extends Define(name, definition) {
+  class ArgDefine(name: String, args: List[String], definition: String) extends Define(name, definition) {
     private def fill(l: List[String]) = l
-    override def parser = (name.r ~ "(" ~ (args.indices.foldLeft(ident)((acc, v) => (("""("([^\n"]|\\")"|[^\n,])""".r <~ ",") ~ acc)^^{s => s._1++s._2})) ~ ")") ^^ { _.toString }
+    def replace(l: List[String]) = args.mkString("|").r.replaceAllIn(definition, _.matched match {
+      case m: String if m != "" => l(args.indexOf(m))
+      case _ => ""
+    })
+    override def parser = (name.r ~ defApplyArgs) ^^ { 
+      case n ~ a => replace(a)
+     }
   }
   def defArgs: Parser[List[String]] = "(" ~> (((ident <~ ",").* ~ ident).?) ~ ")" ^^ {
     case Some(a) ~ _ => a._1 :+ a._2
     case None ~ _ => List()
   }
+  def defApplyArgs: Parser[List[String]] = "(" ~> (((defineP match {
+    case None => """[^\n,]+""".r | string
+    case Some(d) => d | """[^\n,]+""".r | string
+  }).* <~ ",").* ~ (defineP match {
+    case None => """[^\n)]+""".r | string
+    case Some(d) => d | """[^\n)]+""".r | string
+  }).*).? <~ ")" ^^ {
+    case Some(l ~ a) => l.map(_.mkString) :+ a.mkString
+    case None => List()
+  }
   def comment: Parser[String] = """//[^\n]*""".r 
   def string: Parser[String] = """"([^\n"]|\\")*"""".r
   def ident: Parser[String] = """[_a-zA-Z][_a-zA-Z0-9]*""".r
   def definition: Parser[String] = """[^\n]*""".r
-  private def define: Parser[Define] = """#define""".r ~> ident ~ (defArgs.?) ~ definition ^^ {
-    case i ~ Some(a) ~ d => defines += new ArgDefine(i,a,d); defines.last
-    case i ~ None ~ d => defines += new Define(i, d); defines.last
+  def defineP: Option[Parser[String]] = defines.map(_.parser).reduceOption(_|_)
+  private def define: Parser[String] = """#define""".r ~> ident ~ (defArgs.?) ~ definition ^^ {
+    case i ~ Some(a) ~ d => defines += new ArgDefine(i,a,d); s"// #define ${i}(${a.mkString(",")}) ${d}"
+    case i ~ None ~ d => defines += new Define(i, d); s"// #define ${i} ${d}"
   }
-  def defineP: Parser[String] = defines.map(_.parser).reduce(_|_)
-//  def program: Parser[String] = 
-
-  private val defines = HashSet[Define]()
-
-  def test = {
-    val t = new Define("hello", "printf(\"hello world!\")")
-    defines.add(t)
-  }
+  def program: Parser[String] = (string | comment | define | (defineP match {
+    case None => "[^\\n]".r
+    case Some(d) => d | "[^\\n]".r
+  })).* ^^ { _.mkString }
 }
