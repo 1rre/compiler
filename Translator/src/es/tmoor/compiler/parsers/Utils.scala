929d8e89571d01964c4633ea4b181a08d9017cdd
tjm1518 <info@tmoor.es>
2021-01-27 09:26:34 +0000

    remove scala project

--- Translator/src/es/tmoor/compiler/parsers/Utils.scala
+++
@@ -1,9 +0,0 @@
-package es.tmoor.compiler.parsers
-
-import util.parsing.combinator.RegexParsers
-
-trait Utils extends RegexParsers {
-  def comment: Parser[String] = """//[^\n]*""".r 
-  def string: Parser[String] = """"([^\n"]|\\")*"""".r
-  def ident: Parser[String] = """[_a-zA-Z][_a-zA-Z0-9]*""".r
-}
