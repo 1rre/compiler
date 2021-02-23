edfbfe36f952cd1a4100076dcdaf03e62adea05c
tjm1518 <info@tmoor.es>
2021-02-23 14:02:34 +0000

    added register reuse to BIFs

--- src/erl/c_compiler.erl
+++ src/erl/c_compiler.erl
@@ -31,8 +31,8 @@ main([File]) ->
   {ok, Tokens, _} = lexer:string(lists:flatten(Input)),
   {Scan, _Rest} = type_enum:scan(Tokens),
   {ok, Result} = parser:parse(Scan),
-  Proc = var_rename:process(Result),
-  io:fwrite("~n~nResult:~n~n~n~p~n", [Proc]);
+  {ok, _Context, Statement} = var_rename:process(Result),
+  io:fwrite("~n~nResult:~n~n~n~p~n", [Statement]);
 
 
 main(_) -> main(["test/test.c"]).
--- src/erl/codegen/var_rename.erl
+++ src/erl/codegen/var_rename.erl
@@ -1,67 +1,70 @@
 -module(var_rename).
 -export([process/1]).
 
--define(LVARS, proplists:get_value(lvars, Context)).
--define(MVARS, proplists:get_value(lvars, Context)).
--define(LABELS, proplists:get_value(lvars, Context)).
-
 process(Ast) ->
   io:fwrite("Ast:~n~n~p~n~n", [Ast]),
-  process(Ast, [{labels,0},{lvars,0},{mvars,0}]).
-process([], _Context) -> {ok,[],[],0,0};
-process([St|Ast], Context, V_Cnt, L_Cnt) ->
-  {ok,N_Context,St_List,N_V_Cnt,N_L_Cnt} = process(St,Context,V_Cnt,L_Cnt),
-  {ok,N2_Context,N_St_List,N2_V_Cnt,N2_L_Cnt} = process(Ast,N_Context,V_Cnt,L_Cnt),
-  {ok,N2_Context,St_List++N_St_List,N2_V_Cnt,N2_L_Cnt};
+  process(Ast, [{lbcnt,0},{lvcnt,0},{rvcnt,0}]).
+process([], Context) -> {ok,Context,[]};
+process([St|Ast], Context) ->
+  {ok,N_Context,St_List} = process(St,Context),
+  {ok,N2_Context,N_St_List} = process(Ast,N_Context),
+  {ok,N2_Context,St_List++N_St_List};
 
 %% Functions
-process({function,{Return_Type,{{identifier,_,Ident},Args},Statement}}, Context, V_Cnt, L_Cnt) ->
+process({function,{Return_Type,{{identifier,_,Ident},Args},Statement}},Context) ->
   Type = check_typedef(Return_Type, Context),
-  {ok,N_Context,N_St,N_V_Cnt,N_L_Cnt} = process(Statement, Context, V_Cnt, L_Cnt),
-  {ok,N_Context,[{function, Type, Ident, length(Args), N_St}],N_V_Cnt,N_L_Cnt};
-process({function, Fn_Spec}, Context, V_Cnt, L_Cnt) ->
+  {ok,N_Context,N_St} = process(Statement, Context),
+  {ok,N_Context,[{function, Type, Ident, length(Args), N_St}]};
+process({function, Fn_Spec}, Context) ->
   error({unknown_fn_spec,Fn_Spec});
 
 %% Declarations
-process({declaration, O_Type, O_Specs}, Context, V_Cnt, L_Cnt) ->
-  {ok,Ident,N_St,N_V_Cnt,N_L_Cnt} = get_decl_specs(O_Specs, Context, V_Cnt, L_Cnt),
+process({declaration, O_Type, O_Specs}, Context) ->
+  {ok,Ident,N_St} = get_decl_specs(O_Specs, Context),
   Type = check_typedef(O_Type, Context),
-  {ok,[{variable,{Ident,{Type,{y,V_Cnt}}}}|Context],N_St,V_Cnt+1,N_L_Cnt};
+  Rv_Cnt = proplists:get_value(rvcnt, Context),
+  N_Context = [{variable,{Ident,{Type,{y,Rv_Cnt}}}} | increment(rvcnt, Context)],
+  {ok,N_Context,N_St};
 
 %% Built-in Functions
-process({bif, '+',[A,B]}, Context, V_Cnt, L_Cnt) ->
-  {ok,A_Context,A_St,A_V_Cnt,A_L_Cnt} = process(A,Context,V_Cnt,L_Cnt),
-  {ok,B_Context,B_St,B_V_Cnt,B_L_Cnt} = process(B,A_Context,A_V_Cnt+1,A_L_Cnt),
-  Statement = A_St ++ B_St ++ [{add,V_Cnt,[A_V_Cnt,B_V_Cnt]}],
-  {ok,Context,Statement,V_Cnt,B_L_Cnt};
+process({bif, '+',[A,B]}, Context) ->
+  {ok,A_Context,A_St} = process(A,Context),
+  N_A_Context = [{lvcnt,proplists:get_value(lvcnt,Context)+1} | proplists:delete(lvcnt,A_Context)],
+  {ok,B_Context,B_St} = process(B,N_A_Context),
+  V_Cnt = proplists:get_value(lvcnt,Context),
+  Statement = A_St ++ B_St ++ [{add,{x,V_Cnt},[{x,V_Cnt},{x,V_Cnt+1}]}],
+  {ok,B_Context,Statement};
 
 %% Literal values
-process({int_l,_,Val,_Suf}, Context, V_Cnt, L_Cnt) ->
-  {ok,Context,[{move,{x,0},{integer,Val}}],V_Cnt,L_Cnt};
+process({int_l,_,Val,_Suf}, Context) ->
+  V_Cnt = proplists:get_value(lvcnt,Context),
+  {ok,increment(lvcnt,Context),[{move,{x,V_Cnt},{integer,Val}}]};
 
 %% Identifiers
-process({identifier,Ln,Ident}, Context, V_Cnt, L_Cnt) ->
+process({identifier,Ln,Ident}, Context) ->
   Variables = proplists:get_all_values(variable, Context),
   case proplists:get_value(Ident, Variables) of
-    {_Type, X} -> X;
+    {_Type, X} ->
+      V_Cnt = proplists:get_value(lvcnt,Context),
+      {ok,increment(lvcnt,Context),[{move,{x,V_Cnt},X}]};
     Other -> error({Other,Ident,{line,Ln},{context,Context}})
   end;
 
 %% Return
-process({{return,_},Statement}, Context, V_Cnt, L_Cnt) ->
-  {ok,_N_Context,N_St,_N_V_Cnt,N_L_Cnt} = process(Statement, Context, V_Cnt, L_Cnt),
-  {ok,Context,N_St,V_Cnt,N_L_Cnt};
+process({{return,_},Statement}, Context) ->
+  {ok,_N_Context,N_St} = process(Statement, Context),
+  {ok,Context,N_St};
 
 %% Base Case
-process(Part, Context, V_Cnt, L_Cnt) ->
+process(Part, Context) ->
   io:fwrite("Part:~n~p~n~n",[Part]),
-  {ok,[],[],V_Cnt,L_Cnt}.
+  {ok,Context,[]}.
 
-get_decl_specs([{identifier,_,Ident}],Context, V_Cnt, L_Cnt) ->
-  {ok, Ident, [], V_Cnt, L_Cnt};
-get_decl_specs([{{identifier,_,Ident},{'=',_},St}],Context, V_Cnt, L_Cnt) ->
-  {ok, N_Context, N_St, N_V_Cnt, N_L_Cnt} = process(St,Context,V_Cnt,L_Cnt),
-  {ok, Ident, N_St, N_V_Cnt, N_L_Cnt}.
+get_decl_specs([{identifier,_,Ident}],Context) ->
+  {ok, Ident, []};
+get_decl_specs([{{identifier,_,Ident},{'=',_},St}],Context) ->
+  {ok, N_Context, N_St} = process(St,Context),
+  {ok, Ident, N_St}.
 
 
 check_typedef(Type, Context) ->
@@ -76,3 +79,7 @@ get_type([{long,_},{int,_}]) -> int64;
 get_type([{int,_}]) -> int32;
 
 get_type(Type) -> error({unknown_type, Type}).
+
+increment(Key, List) ->
+  Value = proplists:get_value(Key, List),
+  [{Key,Value+1} | proplists:delete(Key,List)].
