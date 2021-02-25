0d64ff2b075dd8358277208fccd32e4cdb03aff1
tjm1518 <info@tmoor.es>
2021-02-25 16:55:26 +0000

    added loops to pointer-compatible ir

--- src/c_compiler.erl
+++ src/c_compiler.erl
@@ -1,12 +1,23 @@
 -module(c_compiler).
 -export([main/1,run_vm/1,run_vm/3]).
 
+main(["-ir", File]) ->
+  {ok, Io_Stream} = file:open(File, [read]),
+  {ok, Input} = read_file(Io_Stream),
+  {ok, Tokens, _} = lexer:string(lists:flatten(Input)),
+  {Scan, _Rest} = type_enum:scan(Tokens),
+  {ok, Result} = parser:parse(Scan),
+  {ok, _Context, Statement} = build_ir:process(Result),
+  io:fwrite("~p~n",[Statement]),
+  {ok, Statement};
+
 main([File]) ->
   {ok, Io_Stream} = file:open(File, [read]),
   {ok, Input} = read_file(Io_Stream),
   {ok, Tokens, _} = lexer:string(lists:flatten(Input)),
   {Scan, _Rest} = type_enum:scan(Tokens),
   {ok, Result} = parser:parse(Scan),
+  io:fwrite("~p~n",[Result]),
   {ok, _Context, Statement} = build_ir:process(Result),
   {ok, Statement};
 
--- src/codegen/build_ir.erl
+++ src/codegen/build_ir.erl
@@ -4,7 +4,6 @@
 -record(state,{lbcnt=0,hpcnt=0,rvcnt=0,lvcnt=0,fn=#{},var=#{},typedef=#{},struct=#{},enum=#{}}).
 
 process(Ast) ->
-  io:fwrite("Ast:~n~p~n~n",[Ast]),
   process(Ast, #state{}).
 
 process([], State) -> {ok, State, []};
@@ -24,6 +23,8 @@ process({identifier,Ln,Ident}, State) ->
       {ok,State,[{move,X,{x,Lv_Cnt}}]};
     Other -> error({Other,Ident,{line,Ln},{state,State}})
   end;
+
+%TODO: Process function calls
 
 process({function,Fn_Specs}, State) ->
   get_fn_specs(Fn_Specs, State);
@@ -47,19 +48,58 @@ process({{'if',_},Predicate,True,False}, State) ->
   Test_Jump = {test,{x,Lv_Cnt},{f,Lb_Cnt+1}},
   {ok,True_State,True_St} = process(True,Test_State),
   False_Label = {label,Lb_Cnt+1},
-  {ok,False_State,False_St} = process(False,Test_State#state{lbcnt=True_State#state.lbcnt}),
+  {ok,False_State,False_St} = process(False,copy_lbcnt(True_State,Test_State)),
+  Rtn_State = copy_lbcnt(False_State,State),
   if
     False_St =:= [] ->
-      Rtn_State = Test_State#state{lbcnt=False_State#state.lbcnt},
       Rtn_St = If_St++[Test_Jump|True_St]++[False_Label],
       {ok,Rtn_State,Rtn_St};
     true ->
       True_Jump = {jump,{f,Lb_Cnt+2}},
       True_Label = {label,Lb_Cnt+2},
-      Rtn_State = Test_State#state{lbcnt=False_State#state.lbcnt},
       Rtn_St = If_St++[Test_Jump|True_St]++[True_Jump,False_Label|False_St]++[True_Label],
       {ok,Rtn_State,Rtn_St}
   end;
+
+process({{while,_},Predicate,Do}, State) ->
+  Lb_Cnt = State#state.lbcnt + 1,
+  Start_Label = {label,Lb_Cnt},
+  {ok,Pred_State,Pred_St} = process(Predicate,State#state{lbcnt=Lb_Cnt+1}),
+  Test_Jump = {test,{x,State#state.lvcnt},{f,Lb_Cnt+1}},
+  {ok,Do_State,Do_St} = process(Do,copy_lvcnt(State,Pred_State)),
+  Jump = {jump,{f,Lb_Cnt}},
+  End_Label = {label,Lb_Cnt + 1},
+  Rtn_State = copy_lbcnt(Do_State,State),
+  Rtn_St = [Start_Label|Pred_St] ++ [Test_Jump|Do_St] ++ [Jump,End_Label],
+  {ok,Rtn_State,Rtn_St};
+
+process({{do,_},Do,Predicate}, State) ->
+  Lb_Cnt = State#state.lbcnt,
+  Start_Label = {label,Lb_Cnt + 1},
+  {ok,Do_State,Do_St} = process(Do,State#state{lbcnt=Lb_Cnt+2}),
+  {ok,Pred_State,Pred_St} = process(Predicate,copy_lbcnt(Do_State,State)),
+  Test_Jump = {test,{x,State#state.lvcnt},{f,Lb_Cnt+2}},
+  Jump = {jump,{f,Lb_Cnt+1}},
+  End_Label = {label,Lb_Cnt + 2},
+  Rtn_State = copy_lbcnt(Pred_State,State),
+  Rtn_St = [Start_Label|Do_St] ++ Pred_St ++ [Test_Jump,Jump,End_Label],
+  {ok,Rtn_State,Rtn_St};
+
+process({{for,_},{Init,Predicate,Update},Loop}, State) ->
+  Lb_Cnt = State#state.lbcnt,
+  Lv_Cnt = State#state.lvcnt,
+  {ok,Init_State,Init_St} = process(Init,State#state{lbcnt=Lb_Cnt+2}),
+  Root_State = Init_State#state{lvcnt=Lv_Cnt},
+  Pred_Label = {label,Lb_Cnt+1},
+  {ok,Pred_State,Pred_St} = process(Predicate,Root_State),
+  Pred_Test = {test,{x,Lv_Cnt},{f,Lb_Cnt+2}},
+  {ok,Loop_State,Loop_St} = process(Loop,copy_lbcnt(Pred_State,Root_State)),
+  {ok,Update_State,Update_St} = process(Update,copy_lbcnt(Loop_State,Root_State)),
+  Jump = {jump,{f,Lb_Cnt+1}},
+  End_Label = {label,Lb_Cnt+2},
+  Next_State = copy_lbcnt(Update_State,State),
+  Next_St = Init_St ++ [Pred_Label|Pred_St] ++ [Pred_Test|Loop_St] ++ Update_St ++ [Jump,End_Label],
+  {ok,Next_State,Next_St};
 
 process({bif,T,[A,B]}, State) ->
   Way_1 = process_bif(T,A,B,State),
@@ -74,13 +114,13 @@ process({{'&',Ln},Raw_St}, State) ->
   case lists:last(Ref_St) of
     {move,Src,Dest} ->
       Next_St = lists:droplast(Ref_St) ++ [{address,Src,Dest}],
-      {ok,Ref_State#state{lvcnt=State#state.lvcnt},Next_St};
+      {ok,copy_lvcnt(State,Ref_State),Next_St};
     {get_heap,Src,Dest} ->
       Next_St = Ref_St ++ [{address,Dest,Dest}],
-      {ok,Ref_State#state{lvcnt=State#state.lvcnt},Next_St};
+      {ok,copy_lvcnt(State,Ref_State),Next_St};
     {address,Src,Dest} ->
       Next_St = Ref_St ++ [{address,Dest,Dest}],
-      {ok,Ref_State#state{lvcnt=State#state.lvcnt},Next_St};
+      {ok,copy_lvcnt(State,Ref_State),Next_St};
     Other -> error({address_error,Other})
     end;
 
@@ -89,18 +129,19 @@ process({{'*',Ln},Raw_St},State) ->
   case lists:last(Ptr_St) of
     {move,Src,Dest} ->
       Next_St = lists:droplast(Ptr_St) ++ [{get_heap,Src,Dest}],
-      {ok,Ptr_State#state{lvcnt=State#state.lvcnt},Next_St};
+      {ok,copy_lvcnt(State,Ptr_State),Next_St};
     {get_heap,Src,Dest} ->
       Next_St = Ptr_St ++ [{get_heap,Dest,Dest}],
-      {ok,Ptr_State#state{lvcnt=State#state.lvcnt},Next_St};
+      {ok,copy_lvcnt(State,Ptr_State),Next_St};
     {address,Src,Dest} ->
       Next_St = Ptr_St ++ [{get_heap,Dest,Dest}],
-      {ok,Ptr_State#state{lvcnt=State#state.lvcnt},Next_St}
+      {ok,copy_lvcnt(State,Ptr_State),Next_St}
   end;
 
 process(Other, State) -> error(Other).
 
-% TODO: Add analysis of `Ident`
+% TODO: Add analysis of `Ident` (eg. returning pointer)
+% TODO: Analysis of args for pass-by-reference?
 get_fn_specs({Raw_Type,{Raw_Ident,Raw_Args},Raw_St}, State) ->
   {ok, Type} = get_type(Raw_Type, State),
   {ok, Ident, Ptr_Ident} = get_ident_specs(Raw_Ident,State),
@@ -108,7 +149,7 @@ get_fn_specs({Raw_Type,{Raw_Ident,Raw_Args},Raw_St}, State) ->
   New_Fn = maps:put(Ident,{Type,length(Raw_Args)},Arg_State#state.fn),
   {ok, N_State, N_St} = process(Raw_St,Arg_State#state{fn=New_Fn}),
   Rtn_St = [{function,Type,Ident,length(Raw_Args),N_St}],
-  {ok,N_State#state{lvcnt=State#state.lvcnt},Rtn_St}.
+  {ok,copy_lvcnt(State,N_State),Rtn_St}.
 
 get_type([{long,_},{long,_},{int,_}],_) -> {ok,int64};
 get_type([{long,_},{int,_}],_) -> {ok,int64};
@@ -116,13 +157,13 @@ get_type([{int,_}],_) -> {ok,int32};
 get_type([{void,_}],_) -> {ok,nil};
 get_type(Type,_) -> {error,{unknown_type, Type}}.
 
-get_decl_specs(Type, [{Raw_Ident,Op,Raw_St}], State) ->
+get_decl_specs(Type, [{Raw_Ident,Op,Raw_St}], State) when Raw_Ident =/= identifier ->
   {ok, Ident, Ptr_Ident} = get_ident_specs(Raw_Ident, State),
   {ok, Mem_State, Mem_St} = allocate_mem(Type, Ptr_Ident, State),
   {ok, Decl_State, Decl_St} = process(Raw_St, Mem_State),
   Rv_Cnt = Decl_State#state.rvcnt,
   New_Var = maps:put(Ident,{Type,{y,Rv_Cnt}},Decl_State#state.var),
-  Next_State = Decl_State#state{var=New_Var,rvcnt=Rv_Cnt+1,lvcnt=State#state.lvcnt},
+  Next_State = (copy_lvcnt(State,Decl_State))#state{var=New_Var,rvcnt=Rv_Cnt+1},
   Next_St = Mem_St ++ Decl_St ++ [{move,{x,State#state.lvcnt},{y,Rv_Cnt}}],
   {ok, Next_State, Next_St};
 
@@ -131,7 +172,7 @@ get_decl_specs(Type, [Raw_Ident], State) ->
   {ok, Mem_State, Mem_St} = allocate_mem(Type, Ident, State),
   Rv_Cnt = Mem_State#state.rvcnt,
   New_Var = maps:put(Ident,{Type,{y,Rv_Cnt}},Mem_State#state.var),
-  Next_State = Mem_State#state{var=New_Var,rvcnt=Rv_Cnt+1,lvcnt=State#state.lvcnt},
+  Next_State = (copy_lvcnt(State,Mem_State))#state{var=New_Var,rvcnt=Rv_Cnt+1},
   {ok,Next_State,Mem_St};
 
 get_decl_specs(Type, Other, State) -> error(Other).
@@ -162,16 +203,16 @@ get_assign_specs('=',[Raw_Ident,Raw_St], State) ->
   end,
   {ok,Ptr_State,Ptr_St} = get_ptr(Ptr_Ident,Ptr,State),
   {ok,Assign_State,Assign_St} = process(Raw_St,State),
-  Next_State = Assign_State#state{lvcnt=State#state.lvcnt},
+  Next_State = copy_lvcnt(State,Assign_State),
   Lv_Cnt = State#state.lvcnt,
   case Ptr_St of
     [] ->
       Next_St = Assign_St++[{move,{x,Lv_Cnt},Ptr}],
-      {ok,Next_State#state{lvcnt=State#state.lvcnt},Next_St};
+      {ok,copy_lvcnt(State,Next_State),Next_St};
     _ ->
       {get_heap,Src,Dest} = lists:last(Ptr_St),
       Next_St = Ptr_St ++ Assign_St ++ [{put_heap,{x,Lv_Cnt},Src}],
-      {ok,Next_State#state{lvcnt=State#state.lvcnt},Next_St}
+      {ok,copy_lvcnt(State,Next_State),Next_St}
   end;
 
 get_assign_specs(Op,[Raw_Ident,Raw_St], State) ->
@@ -196,3 +237,6 @@ process_bif(Type,A,B,State) ->
   Lv_Cnt = State#state.lvcnt,
   Statement = A_St ++ B_St ++ [{Type,{x,Lv_Cnt},[{x,Lv_Cnt},{x,Lv_Cnt+1}]}],
   {ok,B_State,Statement}.
+
+copy_lbcnt(State_1,State_2) -> State_2#state{lbcnt=State_1#state.lbcnt}.
+copy_lvcnt(State_1,State_2) -> State_2#state{lvcnt=State_1#state.lvcnt}.
