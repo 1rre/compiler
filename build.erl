0486cda3eb11f3e354abbdcb44cba34ca97b6d4d
tmoores <info@tmoor.es>
2021-03-09 12:35:17 +0000

    updated makefile & removed CPP

--- build.erl
+++ build.erl
@@ -62,12 +62,6 @@ build_common() ->
   end,
   file:make_dir(".build"),
   process_flag(trap_exit, true),
-  Cxx = get_dep(cxx),
-  Include = get_dep(erts),
-  Cxx_Files = lists:reverse(filelib:wildcard("src/cpp/*.cpp")),
-  io:fwrite("Compiling C++ files: ~p~n",[Cxx_Files]),
-  compile(Cxx, Cxx_Files, Include),
-  wait_exe(1, false),
   Leex_Files = filelib:wildcard("src/parsing/*.xrl"),
   [leex:file(Xrl) || Xrl <- Leex_Files],
   Yecc_Files = filelib:wildcard("src/parsing/*.yrl"),
@@ -77,76 +71,12 @@ build_common() ->
 compile_erl(Erl,Flags) ->
   S = self(),
   spawn_link(fun() ->
-    S ! {self(), {change_ext(Erl, beam),compile:file(Erl,[binary|?ERLC_FLAGS]++Flags)}}
+    S ! {self(), {change_ext(Erl,beam),compile:file(Erl,[binary|?ERLC_FLAGS]++Flags)}}
   end).
 
-%% Collate the dependencies for compilation
-% C++ compiler
-get_dep(cxx) ->
-  case os:getenv("CXX") of
-    false ->
-      io:fwrite("CXX environment variable not set: autodetecting C++ compiler.~n"),
-      find(cxx);
-    Cxx_Env ->
-      case os:find_executable(Cxx_Env) of
-        false ->
-          io:fwrite("CXX environment variable is not in path: autodetecting C++ compiler.~n"),
-          find(cxx);
-        Exec -> Exec
-      end
-  end;
-
-% Erlang runtime system include directory
-get_dep(erts) ->
-  case os:getenv("ERTS_INCLUDE") of
-    false ->
-      io:fwrite("ERTS_INCLUDE environment variable not set: autodetecting ERTS include directory.~n"),
-      find(erts);
-    Include ->
-      case filelib:is_dir(Include) and filelib:is_file(filename:join(Include, "erl_nif.h")) of
-        true -> Include;
-        false ->
-          io:fwrite("ERTS_INCLUDE path is invalid: autodetecting ERTS include directory.~n"),
-          find(erts)
-      end
-  end.
-
-%% Autodetect the dependencies for compilation
-% Erlang runtime system include directory
-find(erts) ->
-  case filelib:wildcard("erts*/include/", code:root_dir()) of
-    [Version | _] ->
-      io:fwrite("Using ERTS Version `~s`.~n", [lists:takewhile(fun (C) -> C /= $/ end, Version)]),
-      filename:join(code:root_dir(), Version);
-    _ -> error("Couldn't find the Erlang Runtime System include directory. Try setting the 'ERTS_INCLUDE' environment variable and ensuring the `erlang_dev` package is installed.")
-  end;
-
-% C++ compiler
-find(cxx) -> find(cxx, ["g++", "clang++"]).
-find(cxx, []) -> error("Couldn't find C++ Compiler. Try setting the 'CXX' environment variable.");
-find(cxx, [Cxx | Rest]) ->
-  case os:find_executable(Cxx) of
-    false -> find(cxx, Rest);
-    Exec ->
-      io:fwrite("Using `~s` as CXX.~n",[Cxx]),
-      Exec
-  end.
-
-%% Asynchronously compile a C++ file
-compile(Cxx, Cxx_Files, Include) ->
-  open_port(
-    {spawn_executable, Cxx},
-     [stderr_to_stdout,
-      {args,
-       ["-I", Include,
-        "-I", "include",
-        "-o", "./.build/erl_nif.so",
-        "-fpic",
-        "-shared"|Cxx_Files]}]).
-
-%% Ensure that all C++ files have compiled correctly
+%% Wait for an executable to exit & ensure the exit code is 0
 wait_exe(0, true) ->
-  error("Compilation had errors.");
+  ("Compilation had errors.");
 wait_exe(0, _) ->
   ok;
 wait_exe(N, Is_Ok) ->
@@ -158,9 +88,8 @@ wait_exe(N, Is_Ok) ->
     _Message -> wait_exe(N, true)
   end.
 
-%% Change a file's 4 character extention, such as '.cpp' or '.xrl' to a different extention
-change_ext(File, beam) -> filename:basename(File, "erl") ++ "beam";
-change_ext(File, so) -> filename:basename(File, "cpp") ++ "so".
+%% Change a erlang file's extension to .beam
+change_ext(File, beam) -> filename:basename(File, "erl") ++ "beam".
 
 %% Create an escript archive with all the compiled modules & make it executable
 escriptise(Bin) ->
--- makefile
+++ makefile
@@ -1,2 +1,21 @@
-% :
-	escript build.erl $@
+ERL_FILES = src/c_compiler.erl .build/lexer.erl .build/parser.erl src/type_enum.erl src/ir_vm.erl src/codegen/ir.erl src/codegen/mips.erl
+ESCRIPT_FILES = ["c_compiler.beam","lexer.beam","parser.beam","type_enum.beam","ir_vm.beam","ir.beam","mips.beam"]
+ESCRIPT_CMD = 'c:cd(".build/"), escript:create("../bin/c_compiler",[shebang,{archive,$(ESCRIPT_FILES),[]}]), halt(0)'
+
+
+bin/c_compiler: clean .build .build/parser.erl .build/lexer.erl
+	erlc -I include/ -o .build/ -DTARGET_ARCH=mips32 $(ERL_FILES)
+	erl -noshell -pa .build/ -eval $(ESCRIPT_CMD)
+	chmod a+x bin/c_compiler
+
+.build:
+	mkdir .build bin
+
+.build/parser.erl:
+	erlc -o .build/parser.erl src/parsing/parser.yrl
+
+.build/lexer.erl:
+	erlc -o .build/lexer.erl src/parsing/lexer.xrl
+
+clean:
+	rm -rf .build bin
